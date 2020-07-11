2ea16ae8ee450565caff914892a3b3541e4842dc
Mr.Ye <1402008814@qq.com>
2020-07-11 21:59:25 +0800

    add a new file

---
+++ src/com/p1/MainClass.java
@@ -0,0 +1,11 @@
+package com.p1;
+
+/**
+ * @author Mr.Ye
+ * @create 2020-07-11 21:47
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
